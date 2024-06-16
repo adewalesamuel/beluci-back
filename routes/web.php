24d3dc85e1c19724b37b76a3dac125c7dfcd7908
Samuel <samuel@macpro-sam.local>
2024-06-16 21:22:27 +0000

    admin + front deploy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin{any}', function () {
+    return view('back');
+})->where('any', '(.*)?');
+
+Route::any('/{any}', function () {
+    return view('front');
+})->where('any', '(.*)?');
