0dc05c018874a8b3968a41f4f7fedf8a52dbf886
Samuel <samroberval@gmail.com>
2024-07-28 11:33:38 +0000

    fix: fixed route memeber

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -117,6 +117,18 @@ class MemberController extends Controller
         return response()->json($data);
     }
 
+    public function profile_show(Request $request)
+    {
+        $member = Auth::getUser($request, Auth::USER);
+
+        $data = [
+            'success' => true,
+            'member' => $member
+        ];
+
+        return response()->json($data);
+    }
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -168,6 +180,52 @@ class MemberController extends Controller
 		$member->idcard_url = $validated['idcard_url'] ?? null;
 		$member->is_validated = $validated['is_validated'] ?? false;
 		$member->member_id = $validated['member_id'] ?? null;
+
+        if (isset($validated['password']))
+            $member->password = $validated['password'] ?? null;
+
+        $member->save();
+
+        $data = [
+            'success'       => true,
+            'member'   => $member
+        ];
+
+        return response()->json($data);
+    }
+
+    public function profile_update(UpdateMemberRequest $request)
+    {
+        $validated = $request->validated();
+        $member = Auth::getUser($request, Auth::USER);
+
+        $member->logo_url = $validated['logo_url'] ?? null;
+		$member->company_name = $validated['company_name'] ?? null;
+		$member->country_name = $validated['country_name'] ?? null;
+		$member->head_office = $validated['head_office'] ?? null;
+		$member->address = $validated['address'] ?? null;
+		$member->website_url = $validated['website_url'] ?? null;
+		$member->fullname = $validated['fullname'] ?? null;
+		$member->creation_date = $validated['creation_date'] ?? null;
+		$member->employee_number = $validated['employee_number'] ?? null;
+		$member->legal_status = $validated['legal_status'] ?? null;
+		$member->share_capital = $validated['share_capital'] ?? null;
+		$member->sector = $validated['sector'] ?? null;
+		$member->other_details = $validated['other_details'] ?? null;
+		$member->company_category = $validated['company_category'] ?? null;
+		$member->representative_fullname = $validated['representative_fullname'] ?? null;
+		$member->position = $validated['position'] ?? null;
+		$member->nationality = $validated['nationality'] ?? null;
+		$member->email = $validated['email'] ?? null;
+		$member->phone_number = $validated['phone_number'] ?? null;
+		$member->sales_representative_fullname = $validated['sales_representative_fullname'] ?? null;
+		$member->sales_representative_position = $validated['sales_representative_position'] ?? null;
+		$member->sales_representative_email = $validated['sales_representative_email'] ?? null;
+		$member->sales_representative_phone_number = $validated['sales_representative_phone_number'] ?? null;
+		$member->cover_letter_url = $validated['cover_letter_url'] ?? null;
+		$member->photo_url = $validated['photo_url'] ?? null;
+		$member->commercial_register_url = $validated['commercial_register_url'] ?? null;
+		$member->idcard_url = $validated['idcard_url'] ?? null;
 
         if (isset($validated['password']))
             $member->password = $validated['password'] ?? null;
