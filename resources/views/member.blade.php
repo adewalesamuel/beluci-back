25fa1be95de37f946f6d6dccbd45f88a306d6b5b
Samuel <samroberval@gmail.com>
2024-07-28 12:20:53 +0000

    member + admin deploy

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/member/vite.svg" />
+    <link rel="icon" type="image/svg+xml" href="/member/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Espace Membre</title>
-    <script type="module" crossorigin src="/member/assets/index-Ch4YET9f.js"></script>
+    <script type="module" crossorigin src="/member/assets/index-k20DFhtr.js"></script>
     <link rel="stylesheet" crossorigin href="/member/assets/index-CxI_eIxn.css">
   </head>
   <body>
