59df7cc9b795aa079e466350aa827d944446554b
Samuel <samroberval@gmail.com>
2024-10-05 06:44:54 +0000

    member front deploy

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/svg+xml" href="/member/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Espace Membre</title>
-    <script type="module" crossorigin src="/member/assets/index-D60e76Vt.js"></script>
+    <script type="module" crossorigin src="/member/assets/index-mRU6Vfc1.js"></script>
     <link rel="stylesheet" crossorigin href="/member/assets/index-CxI_eIxn.css">
   </head>
   <body>
