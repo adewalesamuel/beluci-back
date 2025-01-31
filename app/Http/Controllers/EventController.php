c5995d7513b0033883f911d9547f4e25daf1db43
Samuel <samroberval@gmail.com>
2025-01-31 00:10:49 +0000

    fix sort event by created date

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -19,7 +19,7 @@ class EventController extends Controller
     public function index(Request $request)
     {
     	$events = Event::where('id', '>', -1)
-        ->orderBy('date', 'desc');
+        ->orderBy('created_at', 'desc');
 
         if ($request->input('page') == null ||
             $request->input('page') == '') {
